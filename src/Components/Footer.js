359854e971eff78d9da9ed699e9b7553bc502ae8
Wade Butler <wadebutler2@gmail.com>
2020-04-22 12:35:48 -0400

    initial commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer>
+            <p>Made by <a href="http://wadebutler.ca/">Wade Butler</a> Powered by <a href="https://tech.yandex.com/translate/">Yandex Translate</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
