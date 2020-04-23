b651155842c03cc298e7f3da8317ebf2b9370754
Wade Butler <wadebutler2@gmail.com>
2020-04-22 21:33:55 -0400

    added axios

--- src/Components/Translator.js
+++ src/Components/Translator.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function Translator() {
-    return (
-        <main>
-            <label htmlFor="selectFrom">Language:
-            <select name="selectFrom" id="selectFrom">
-                    <option selected="selected">Translate to?</option>
-                    <option value="English">English</option>
-                </select>
-            </label>
-            <label htmlFor="">
-                <textarea placeholder="Hello" name="textFrom" id="textFrom" cols="30" rows="10"></textarea>
-            </label>
+// URLS FOR YANDEX API
+const translateURL = "https://translate.yandex.net/api/v1.5/tr.json/translate";
+const languagesURL = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs';
+const detectURL = 'https://translate.yandex.net/api/v1.5/tr.json/detect';
+const apiKey = 'trnsl.1.1.20200413T224443Z.493dc6e5d26c9b52.1719c0c35f9ae9338654d19067f66d7026c049be';
 
-            <button>Translate</button>
-        </main>
-    );
+class Translator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            language: {},
+        }
+    }
+
+    componentDidMount = () => {
+        axios.get(languagesURL, {
+            params: {
+                key: apiKey,
+                ui: 'en',
+            }
+        }).then((results) => {
+            console.log(results.data.langs)
+        })
+    }
+
+    render() {
+        return (
+            <main>
+                <label htmlFor="toTranslate">Enter Text to
+                    <textarea placeholder="Hello" name="toTranslate" id="toTranslate" cols="30" rows="10"></textarea>
+                </label>
+
+                <label htmlFor="selectFrom">Language:
+                    <select name="selectFrom" id="selectFrom">
+                        <option selected="selected">Translate to?</option>
+                    </select>
+                </label>
+
+                <button>Translate</button>
+            </main>
+        )
+    }
 }
 
 export default Translator;
